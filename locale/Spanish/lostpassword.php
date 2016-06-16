1b114d6319d5b53e38469ae74b7d3fd08262ee38
Talocha <mateofds@hotmail.com>
2016-06-16 17:39:42 +0200

    Create lostpassword.php

---
+++ locale/Spanish/lostpassword.php
@@ -0,0 +1,26 @@
+<?php
+$locale['400'] = "Nueva Contraseña";
+$locale['401'] = "Se ha enviado un correo de confirmación a tu dirección de email.";
+$locale['402'] = "Tu nueva contraseña ha sido enviada a tu dirección de email.";
+$locale['403'] = "Volver al Sitio";
+$locale['404'] = "No se ha podido encontrar la dirección de email que has indicado.";
+$locale['405'] = "La dirección de email que has indicado no es válida.";
+$locale['406'] = "Vuelve a intentarlo.";
+$locale['407'] = "Indica la dirección de email con la que registraste tu cuenta.<br />
+Se creará una nueva contraseña y se enviará a esa dirección.";
+$locale['408'] = "Enviar";
+$locale['409'] = "Nueva Contraseña para ";
+$locale['410'] = "Hola [USER_NAME].\n
+Ha sido solicitada una nueva contraseña para acceder a tu cuenta de ".$settings['sitename'].".\n
+Si tú no has solicitado esta nueva contraseña, simplemente borra este email.\n
+Por el contrario, si la has solicitado tú, debes pulsar el siguiente enlace para confirmarlo:\n
+[NEW_PASS_LINK]\n
+Saludos,
+".$settings['siteusername'];
+$locale['411'] = "Hola [USER_NAME].\n
+Tu nueva contraseña para acceder a tu cuenta de ".$settings['sitename']." es:\n
+[NEW_PASS]\n
+Saludos,
+".$settings['siteusername'];
+$locale['412'] = "Se ha producido un error. Vuelve a inténtalo.";
+?>

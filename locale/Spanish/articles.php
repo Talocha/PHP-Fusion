cb19e63bb09e4da52358a35f2a168d5aaa5219b2
Talocha <mateofds@hotmail.com>
2016-06-16 17:32:11 +0200

    Create articles.php

---
+++ locale/Spanish/articles.php
@@ -0,0 +1,8 @@
+<?php
+$locale['400'] = "Artículos";
+$locale['401'] = "No se han creado categorías de artículos";
+$locale['402'] = "<b>NUEVO</b>";
+$locale['403'] = "No hay artículos en esta categoría";
+$locale['404'] = "Artículos";
+$locale['405'] = "Artículos";
+?>

84fc1cbe838b30cfaf9929588b383c5978840d15
Talocha <mateofds@hotmail.com>
2016-06-16 17:37:23 +0200

    Create faq.php

---
+++ locale/Spanish/faq.php
@@ -0,0 +1,8 @@
+<?php
+$locale['400'] = "FAQ (Preguntas Frecuentes)";
+$locale['401'] = "FAQ";
+$locale['402'] = "Arriba";
+// Notices
+$locale['410'] = "No se han creado categorías de FAQ.";
+$locale['411'] = "No hay preguntas en esta categoría.";
+?>
